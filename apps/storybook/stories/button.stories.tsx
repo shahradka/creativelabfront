2bf9082bd8567a42024e323b937f94f289db05f2
Shahrad Ka <shahrad.kashany@gmail.com>
2025-12-19 11:22:12 +0100

    Refactor code structure for improved readability and maintainability

---
+++ apps/storybook/stories/button.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@workspace/ui/components/button"
+import "@workspace/ui/globals.css"
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {
+    type: {
+      control: { type: "radio" },
+      options: ["button", "submit", "reset"],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: (props) => (
+    <Button
+      {...props}
+      onClick={(): void => {
+        // eslint-disable-next-line no-alert -- alert for demo
+        alert("Hello from Turborepo!");
+      }}
+    >
+      Hello
+    </Button>
+  ),
+  name: "Button",
+  args: {
+    children: "Hello",
+    type: "button",
+    variant: "default",
+  },
+};
