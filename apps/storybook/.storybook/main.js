d3d93e3bfd52f44304e42d4ce94912b72a9a7a34
Shahrad Ka <shahrad.kashany@gmail.com>
2026-01-12 18:11:14 +0100

    fix: update main.js and package.json for improved Vite configuration and dev script

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,17 +1,17 @@
-import { dirname, join, resolve } from "path";
+import { dirname, join, resolve } from 'path';
 
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 const config = {
-  stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+  stories: ['../stories/*.stories.tsx', '../stories/**/*.stories.tsx'],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
 
@@ -21,12 +21,14 @@ const config = {
     // customize the Vite config here
     return {
       ...config,
-      define: { "process.env": {} },
+      host: true,
+      allowedHosts: ['dev.creativelab.top'],
+      define: { 'process.env': {} },
       resolve: {
         alias: [
           {
-            find: "ui",
-            replacement: resolve(__dirname, "../../../packages/ui/"),
+            find: 'ui',
+            replacement: resolve(__dirname, '../../../packages/ui/'),
           },
         ],
       },
